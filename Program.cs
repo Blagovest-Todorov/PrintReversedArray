4c9b3b7e2d2079866983bde2a7d86950d8a9a1b9
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-25 21:44:55 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ReadArrayStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrString = Console.ReadLine().Split().ToArray(); // read String Line /remove the space between strings and convert to Array
+            string[] reversedArr = new string[arrString.Length];
+
+            for (int i = 0; i < arrString.Length; i++)
+            {                
+                reversedArr[reversedArr.Length - 1 - i] = arrString[i];                
+            }
+            foreach (string item in reversedArr)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+    }
+}
